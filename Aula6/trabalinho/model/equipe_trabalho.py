a70ec57bcdc8af93b88aa75bb8193988a0970aa7
Elenice Paterno <54104422+elenicepaterno@users.noreply.github.com>
2019-12-10 10:16:27 -0300

    Update EquipeTrabalho

--- Aula6/trabalinho/model/equipe_trabalho.py
+++ Aula6/trabalinho/model/equipe_trabalho.py
@@ -1,12 +1,17 @@
-from funcionario import Funcionario
+import sys
+sys.path.append("C:/Users/900213/Desktop/trabalinho/")
 
+# Importação da classe Funcionário, que recebe os dados profissionais dos funcionarios
+from trabalinho.model.funcionario import Funcionario
+
+# Classe herdada de Funcionário, que irá receber o nome da equipe que o funcionário trabalhará
 class EquipeTrabalho(Funcionario):
-    __nome = ''
+    __nome_equipe = ''
 
     #getter
-    def get_nome(self):
-        return self.__nome
+    def get_nome_equipe(self):
+        return self.__nome_equipe
 
     # setter
-    def set_nome(self, nome):
-        self.__nome = nome
+    def set_nome_equipe(self, nome_equipe):
+        self.__nome_equipe = nome_equipe
