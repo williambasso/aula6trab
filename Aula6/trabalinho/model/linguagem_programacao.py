7e2874919cf7b29196a740c1b40b2f99e552cd69
Elenice Paterno <54104422+elenicepaterno@users.noreply.github.com>
2019-12-10 10:17:39 -0300

    Update LinguagemProgramacao

--- Aula6/trabalinho/model/linguagem_programacao.py
+++ Aula6/trabalinho/model/linguagem_programacao.py
@@ -1,18 +1,22 @@
+import sys
+sys.path.append("C:/Users/900213/Desktop/trabalinho/")
+
+# Importação da classe Funcionario, que recebe os dados profissionais de cada funcionário
 from trabalinho.model.funcionario import Funcionario
 
+# Classe herdada de Funcionario, que recebe o nome das linguagens que os funcionários usam/usarão
 class LinguagemProgramacao(Funcionario):
     __id = 0
     __nome_linguagem = ''
 
-    #getter
+   # Método getter, usados para buscarem/recuperarem os dados
     def get_id(self):
         return self.__id
 
     def get_nome_linguagem(self):
         return self.__nome_linguagem
     
-    #setters
-
+   # Método setter, usados para setarem/alterarem os dados
     def set_id(self, id):
         self.__id = id
 
