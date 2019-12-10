fc26e71e004afba2a7899b2e3e7721a405b610c9
Elenice Paterno <54104422+elenicepaterno@users.noreply.github.com>
2019-12-10 10:17:00 -0300

    Update Funcionário

--- Aula6/trabalinho/model/funcionario.py
+++ Aula6/trabalinho/model/funcionario.py
@@ -1,15 +1,26 @@
-from trabalinho.model.pessoa import Pessoa
-# import sys
-# sys.path.append('../dao')
+import sys
+sys.path.append("C:/Users/900213/Desktop/trabalinho/")
 
+# Importação da classe Pessoa, que recebe os dados pessoais dos funcionários
+from trabalinho.model.pessoa import Pessoa
+
+# Classe herdada de Pessoa, que recebe os dados profissionais privados de cada funcionário
 class Funcionario(Pessoa):
+    __linguagem = ''
+    __equipe = ''
     __salario = 0
     __cargo = ''
     __pis = 0
 
-    #getter
+    # Método getter, usados para buscarem/recuperarem os dados
     def get_salario(self):
         return self.__salario
+    
+    def get_linguagem(self):
+        return self.__linguagem
+    
+    def get_equipe(self):
+        return self.__equipe
 
     def get_cargo(self):
         return self.__cargo
@@ -17,10 +28,15 @@ class Funcionario(Pessoa):
     def get_pis(self):
         return self.__pis
     
-    #setters
-
+    # Método setter, usados para setarem/alterarem os dados
     def set_salario(self, salario):
         self.__salario = salario
+        
+    def set_linguagem(self, linguagem):
+        self.__linguagem = linguagem
+        
+    def set_equipe(self, equipe):
+        self.__equipe = equipe
 
     def set_cargo(self, cargo):
         self.__cargo = cargo
