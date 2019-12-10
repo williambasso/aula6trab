ce6962206674c9b73c3ca614db0a9fcafce2d8d6
Elenice Paterno <54104422+elenicepaterno@users.noreply.github.com>
2019-12-10 09:21:33 -0300

    Update BaseDao

--- Aula6/trabalinho/dao/basedao.py
+++ Aula6/trabalinho/dao/basedao.py
@@ -1,11 +1,15 @@
+# Importação do banco de dados
 import MySQLdb
 
+# Classe que inicializa a conexão e o cursor com o banco de dados
 class BaseDao:
     def __init__(self):
         # conectando BD
         self.conn = MySQLdb.connect(
             host='mysql.topskills.study', database='topskills04', user='topskills04', passwd='Elenice2019')
         self.cursor = self.conn.cursor()
+        
+# Metodos que realizaram ações no banco
 
     def listar(self, sqll):
         self.cursor.execute(sqll)
